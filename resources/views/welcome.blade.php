d255bdc6c97fa7861351802948a2a6e8dabdca74
Davide Pio Dicandia <dicandiadavide@gmail.com>
2023-05-25 15:46:56 +0200

    inserite cards in homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,56 @@
                 <div class="col-md-6 text-center">
                     <p class="fontMh fw-bold">Presto.it</p>
                     <p class="fontMh">Vendi. Compra. Ricomincia da capo.</p>
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-6 d-flex justify-content-center">
+                        <button class="btnMh">SHOP NOW</button>
+                    </div>
+                </div>
             </div>
-            <div class="row d-flex justify-content-center">
-                <div class="col-6 d-flex justify-content-center">
-                    <button class="btnMh">SHOP NOW</button>
+    </header>
+
+    <section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-3">
+                <div class="card">
+                    <div class="card-details">
+                        <p class="text-title">Card title</p>
+                        <p class="text-body">Here are the details of the card</p>
+                    </div>
+                    <button class="card-button">More info</button>
+                </div>
+            </div>
+            <div class="col-12 col-md-3">
+                <div class="card">
+                    <div class="card-details">
+                        <p class="text-title">Card title</p>
+                        <p class="text-body">Here are the details of the card</p>
+                    </div>
+                    <button class="card-button">More info</button>
+                </div>
+            </div>
+            <div class="col-12 col-md-3">
+                <div class="card">
+                    <div class="card-details">
+                        <p class="text-title">Card title</p>
+                        <p class="text-body">Here are the details of the card</p>
+                    </div>
+                    <button class="card-button">More info</button>
+                </div>
+            </div>
+            <div class="col-12 col-md-3">
+                <div class="card">
+                    <div class="card-details">
+                        <p class="text-title">Card title</p>
+                        <p class="text-body">Here are the details of the card</p>
+                    </div>
+                    <button class="card-button">More info</button>
                 </div>
             </div>
         </div>
-    </header>
+    </div>
+    </section>
 
 </x-layout>
