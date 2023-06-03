a1b8c8d281700068070f0f67b6a9774e89e7cd40
Davide Pio Dicandia <dicandiadavide@gmail.com>
2023-06-03 13:04:32 +0200

    installato livewire

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,22 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @livewireStyles
     @vite (['resources/css/app.css', 'resources/js/app.js'])
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Quicksand:wght@300;400;500&display=swap"
         rel="stylesheet">
-    <title>{{$title ?? 'Presto.it'}}</title>
+    <title>{{ $title ?? 'Presto.it' }}</title>
 </head>
 
 <body>
     <x-navbar />
 
-    {{$slot}}
+    {{ $slot }}
 
     <x-footer />
-
+    @livewireScripts
 </body>
 
 </html>
